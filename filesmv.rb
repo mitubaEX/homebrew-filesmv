8a035d9b2d09761cf9b9a26d620583c34eb08dd1
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2017-09-18 01:45:23 +0900

    initial

---
+++ filesmv.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+HOMEBREW_FILEMV_VERSION='1.0'
+class Pt < Formula
+  homepage 'https://github.com/mitubaEX/filesmv'
+  url 'https://github.com/mitubaEX/filesmv.git', :tag => "v#{HOMEBREW_PT_VERSION}"
+  version HOMEBREW_PT_VERSION
+  head 'https://github.com/mitubaEX/filesmv.git', :branch => 'master'
+
+  depends_on 'go' => :build
+  # depends_on 'hg' => :build
+
+  def install
+    ENV['GOPATH'] = buildpath
+    # system 'go', 'get', 'github.com/shiena/ansicolor'
+    # system 'go', 'get', 'github.com/monochromegane/terminal'
+    # system 'go', 'get', 'github.com/jessevdk/go-flags'
+    # system 'go', 'get', 'code.google.com/p/go.text/transform'
+    # mkdir_p buildpath/'src/github.com/mitubaEX/filesmv'
+    # ln_s buildpath/'search', buildpath/'src/github.com/mitubaEX/filesmv/.'
+    system 'go', 'build', '-o', 'filesmv'
+    bin.install 'filesmv'
+  end
+end
